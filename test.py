310946841356c65e3943481c87fa3c75e6413a3a
10jin-yidiandian <74892723+10jin-yidiandian@users.noreply.github.com>
2024-04-28 22:26:06 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from valid import test_accuracy
 import pickle
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -12,4 +13,22 @@ if __name__ == '__main__':
     accu = test_accuracy(param)
     print(f'加载模型在测试集上的测试准确率为{accu}')
     print('参数可视化：')
-    print('第一层网络：','\n',param[0],'\n','第二层网络：','\n',param[1])
+    print(len((param[0]['w']@param[1]['w']).reshape((28,28,10))))
+    image_matrices = (param[0]['w']@param[1]['w']).reshape((28,28,10))
+    fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(5, 50))
+
+    # 循环遍历每个矩阵并在相应的子图中显示
+    for i in range(10):
+        l = i//5
+        w = i%5
+        axes[l,w].imshow(image_matrices[:,:,i], cmap='gray')
+        axes[l,w].axis('off')  # 关闭坐标轴
+
+    # 调整布局以防止重叠
+    plt.tight_layout()
+
+    # 保存整个图像
+    plt.savefig('combined_images.png')
+
+    # 显示图像
+    plt.show()
